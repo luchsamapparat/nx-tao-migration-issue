1c3aabca5bc4c14acb611cbf8959b4435a006ce2
Marvin Luchs <marvin.luchs@holisticon.de>
2021-01-04 09:12:43 +0100

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-tao-migration-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
